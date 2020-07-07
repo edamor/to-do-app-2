ed9ed1f1b74c01ec1178439dfbadd47a6bf37ac1
amor.edlouise <amor.edlouise@gmail.com>
2020-07-07 10:40:12 +0800

    Updates

---
+++ precache-manifest.2689483252839a7c665e9372765fb917.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ad3ced07ab92156e4c62555b73cb42",
+    "url": "/to-do-app-2/index.html"
+  },
+  {
+    "revision": "da3837489078049f70b8",
+    "url": "/to-do-app-2/static/css/main.36a45a45.chunk.css"
+  },
+  {
+    "revision": "89a154ed2c1102f1de81",
+    "url": "/to-do-app-2/static/js/2.4d7ebb66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to-do-app-2/static/js/2.4d7ebb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da3837489078049f70b8",
+    "url": "/to-do-app-2/static/js/main.6f2fecd2.chunk.js"
+  },
+  {
+    "revision": "91f0140a208ff07b3e63",
+    "url": "/to-do-app-2/static/js/runtime-main.1729d156.js"
+  },
+  {
+    "revision": "623d3aa396fffb7969a3ef774cdd18aa",
+    "url": "/to-do-app-2/static/media/add.623d3aa3.png"
+  },
+  {
+    "revision": "d1826015d458a030a28f932535bc384d",
+    "url": "/to-do-app-2/static/media/check.d1826015.png"
+  },
+  {
+    "revision": "2108b72f39a26171f71a7d8be0321c46",
+    "url": "/to-do-app-2/static/media/garbage.2108b72f.png"
+  },
+  {
+    "revision": "bf604bf35f6ce431fae83fba75298dd0",
+    "url": "/to-do-app-2/static/media/play.bf604bf3.png"
+  },
+  {
+    "revision": "2148f3800e7e5ea40b8bf40951e27e42",
+    "url": "/to-do-app-2/static/media/undo.2148f380.png"
+  }
+]);
